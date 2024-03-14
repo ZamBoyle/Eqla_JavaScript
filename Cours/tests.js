ff996d5f9f657cfe38d3b3a2202487550d54905b
Johnny Piette <johnny.piette@gmail.com>
2024-03-14 21:42:29 +0100

    Updates

--- Cours/tests.js
+++ Cours/tests.js
@@ -1,1 +1,9 @@
-document.getElementById("titre").innerHTML = "J'ai changÉ le titre";
+let prenom = "Johnny";
+let nom = "Piette";
+let age = 30; //Faut bien trouver des moyens pour se rajeunir ;-)
+
+// Avec l'interpolation de chaînes de caractères
+console.log(`Tu t'appelles ${prenom} ${nom} a ${age} ans.`); // Johnny Piette a 30 ans.
+
+// Avec l'opérateur de concaténation +
+console.log('Tu t\'appelles ');
